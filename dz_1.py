d7b69d6f1ce696a13787a5fc118fec219c587eba
mathildochka <themathildochka@gmail.com>
2023-07-02 18:59:37 +0300

    Для проверки полиндромности строка перебиралсь в обратном порядке и значение присваивалось переменной, которая сравнивалась с оригиналом. В случае совпадения функцмя на выходе передавала True, и False - нет.

--- dz_1.py
+++ dz_1.py
@@ -1,1 +1,13 @@
-print('Hello, world!')
+def func_verification_palindrom(p):
+    new_p = ""
+    for sym in reversed(p):
+        new_p = new_p + sym
+    if p == new_p:
+        conclusion = True
+    else:
+        conclusion = False
+    return conclusion
+
+palindrom = input('Введите образец для проверки падижром или нет:')
+
+print(func_verification_palindrom(palindrom))
